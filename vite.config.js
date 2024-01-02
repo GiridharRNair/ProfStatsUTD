ccdff84bff70737b20a1008659388684e805636c
Giridhar Nair <Giridhar.r.nair@gmail.com>
2024-01-01 19:21:55 -0600

    HMR, prettier, and update readme

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./public/manifest.json";
+import fs from "fs";
+import path from "path";
+
+const manifestPath = path.resolve(__dirname, "./public/manifest.json");
+const packageJsonPath = path.resolve(__dirname, "package.json");
+
+const existingManifest = JSON.parse(fs.readFileSync(manifestPath), "utf-8");
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath),  "utf-8");
+
+const dynamicManifest = {
+    ...existingManifest,
+    version: packageJson.version,
+};
 
 export default defineConfig({
-    plugins: [react(), crx({ manifest })],
+    plugins: [react(), crx({ manifest: dynamicManifest })],
+    server: { port: 5173 },
 });
