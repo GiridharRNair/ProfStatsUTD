2ee1655994aa530b627dc2fa478f0d63b9a0661d
Giridhar Nair <Giridhar.r.nair@gmail.com>
2024-01-21 13:21:54 -0600

    Improve rate us modal, ci/cd, add db to gitignore

--- src/components/RateUsModal.jsx
+++ src/components/RateUsModal.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, Button, Image, VStack, Icon, Heading, Center } from "@chakra-ui/react";
 import { FcFeedback } from "react-icons/fc";
 
 function RateUsModal() {
-    const [rateUsModalOpen, setRateUsModalOpen] = useState(localStorage.getItem("LastInputData") && !localStorage.getItem("hasRated") && Math.random() < 1.25);
+    const [rateUsModalOpen, setRateUsModalOpen] = useState(localStorage.getItem("LastInputData") && !localStorage.getItem("hasRated") && Math.random() < 0.25);
+    const [buttonTextColor, setButtonTextColor] = useState("#333"); 
+
+    const generateGradientColor = () => {
+        const time = new Date().getTime();
+        const red = Math.sin(0.001 * time) * 127 + 128;
+        const green = Math.sin(0.001 * time + 4) * 127 + 128;
+        const blue = Math.sin(0.001 * time + 8) * 127 + 128;
+        return `rgb(${red}, ${green}, ${blue})`;
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setButtonTextColor(generateGradientColor());
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <Modal isOpen={rateUsModalOpen} onClose={() => setRateUsModalOpen(false)} size="md">
@@ -37,6 +54,8 @@ function RateUsModal() {
                             }}
                             variant="outline"
                             width={250}
+                            borderColor={buttonTextColor}
+                            textColor={buttonTextColor} 
                         >
                             Leave a Review
                         </Button>
