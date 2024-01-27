cfdf3260decb343face130b905cd569a27b19ea9
Giridhar Nair <Giridhar.r.nair@gmail.com>
2024-01-26 23:23:21 -0600

    Center content in chrome extension full page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { ColorModeScript, extendTheme, ChakraProvider } from "@chakra-ui/react";
 import App from "./App.jsx";
+import "./styles/index.css";
 
 const config = {
     initialColorMode: "system",
